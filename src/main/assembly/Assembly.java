db8e7fc7ea0a297c40429b9b5193e2ad70329f79
leikang123 <1013626230@qq.com>
2022-03-21 12:48:50 +0800

    add package file

---
+++ src/main/assembly/Assembly.java
@@ -0,0 +1,25 @@
+package src.main.assembly;
+abstract public class Assembly {
+    abstract public String toSource(SymbolTable table);
+    abstract public String dump();
+
+    public boolean isInstruction() {
+        return false;
+    }
+
+    public boolean isLabel() {
+        return false;
+    }
+
+    public boolean isDirective() {
+        return false;
+    }
+
+    public boolean isComment() {
+        return false;
+    }
+
+    public void collectStatistics(Statistics stats) {
+        
+    }
+}
