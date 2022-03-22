e5dccb11d63c9c21bfcb36e2ce0c5eb5a1de097d
leikang123 <1013626230@qq.com>
2022-03-22 19:06:13 +0800

    update file

--- src/main/assembly/Operand.java
+++ src/main/assembly/Operand.java
@@ -1,24 +1,24 @@
 package main.assembly;
-
+// 操作指令的抽象类实现指令平台接口
 public abstract class Operand implements OperandPattern {
     abstract public String toSource(SymbolTable table);
     abstract public String dump();
-
+   // 寄存器
     public boolean isRegister() {
         return false;
     }
-
+   // 内存引用
     public boolean isMemoryReference() {
         return false;
     }
-
+    // 立即数功能
     public IntegerLiteral integerLiteral() {
         return null;
     }
 
     abstract public void collectStatistics(Statistics stats);
 
-    // default implementation
+    // 匹配操作数
     public boolean match(Operand operand) {
         return equals(operand);
     }
