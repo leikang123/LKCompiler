3b21406529c2c818aa986cd84df5133bd13263c3
leikang123 <1013626230@qq.com>
2022-03-23 18:57:47 +0800

    add Register code

---
+++ src/main/system/x86/Register.java
@@ -0,0 +1,73 @@
+package main.system.x86;
+
+public class Register extends main.assembly.Register{
+    public RegisterClass class;
+    public Type type;
+    
+    //构造方法
+    public Register(RegisterClass class,Type type){
+        this.class = class;
+        this.type = type;
+
+    }
+    public Register forType(Type t) {
+        return new Register(class, t);
+    }
+
+    public boolean isRegister() { 
+        return true;
+     }
+
+    public boolean equals(Object other) {
+        return (other instanceof Register) && equals((Register)other);
+    }
+
+    /** 大小不同的匹配. */
+    public boolean equals(Register reg) {
+        return class.equals(reg.class);
+    }
+
+    public int hashCode() {
+        return class.hashCode();
+    }
+
+    RegisterClass registerClass() {
+        return class;
+    }
+
+    String baseName() {
+        return class.toString().toLowerCase();
+    }
+
+    public String toSource(SymbolTable table) {
+        // GNU assembler dependent
+        return "%" + typedName();
+    }
+
+    private String typedName() {
+        switch (type) {
+        case INT8: return lowerByteRegister();
+        case INT16: return baseName();
+        case INT32: return "e" + baseName();
+        case INT64: return "r" + baseName();
+        default:
+            throw new Error("unknown register Type: " + type);
+        }
+    }
+
+    private String lowerByteRegister() {
+        switch (class) {
+        case AX:
+        case BX:
+        case CX:
+        case DX:
+            return baseName().substring(0, 1) + "l";
+        default:
+            throw new Error("does not have lower-byte register: " + _class);
+        }
+    }
+
+    public String dump() {
+        return "(Register " +class.toString() + " " + type.toString() + ")";
+    }
+}
