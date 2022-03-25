6071ff329dd55ea3d09b79d7dfd0d1d8f8dd3188
leikang123 <1013626230@qq.com>
2022-03-25 18:10:53 +0800

    add file

---
+++ src/main/system/GNUAssembler.java
@@ -0,0 +1,22 @@
+package main.system;
+import java.util.List;
+import java.util.ArrayList;
+
+public  class GNUAssembler implements Assembler {
+    ErrorHandler errorHandler;
+
+    GNUAssembler(ErrorHandler h) {
+        this.errorHandler = h;
+    }
+
+    public void assemble(String srcPath, String destPath,
+                            AssemblerOptions opts) throws IPCException {
+        List<String> cmd = new ArrayList<String>();
+        cmd.add("as");
+        cmd.addAll(opts.args);
+        cmd.add("-o");
+        cmd.add(destPath);
+        cmd.add(srcPath);
+        CommandUtils.invoke(cmd, errorHandler, opts.verbose);
+    }
+}
