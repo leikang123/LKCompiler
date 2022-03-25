4fbec7f8c5b21e95d66868496c8cef3b4dd97f7f
leikang123 <1013626230@qq.com>
2022-03-25 18:10:26 +0800

    add file

---
+++ src/main/system/CodeGeneratorOptions.java
@@ -0,0 +1,52 @@
+package main.system;
+
+public class CodeGeneratorOptions {
+    protected int optimizeLevel;
+    protected boolean generatePIC;
+    protected boolean generatePIE;
+    protected boolean verboseAsm;
+
+    public CodeGeneratorOptions() {
+        optimizeLevel = 0;
+        generatePIC = false;
+        generatePIE = false;
+        verboseAsm = false;
+    }
+
+    public void setOptimizationLevel(int level) {
+        this.optimizeLevel = level;
+    }
+
+    public int optimizeLevel() {
+        return optimizeLevel;
+    }
+
+    public void generateVerboseAsm() {
+        this.verboseAsm = true;
+    }
+
+    public boolean isVerboseAsm() {
+        return verboseAsm;
+    }
+
+    public boolean isPositionIndependent() {
+        return generatePIC || generatePIE;
+    }
+
+    public void generatePIC() {
+        this.generatePIC = true;
+    }
+
+    public boolean isPICRequired() {
+        return generatePIC;
+    }
+
+    public void generatePIE() {
+        this.generatePIE = true;
+    }
+
+    public boolean isPIERequired() {
+        return generatePIE;
+    }
+    
+}
