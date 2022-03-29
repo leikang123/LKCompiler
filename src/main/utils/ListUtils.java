c753af000afdf25b4a30cb31f06b2757e44304a1
leikang123 <1013626230@qq.com>
2022-03-29 19:01:03 +0800

    add  code

--- src/main/utils/ListUtils.java
+++ src/main/utils/ListUtils.java
@@ -1,9 +1,10 @@
 package main.utils;
 import java.io.*;
 import java.util.*;
-
+// 抽象列表工具类
 public abstract class ListUtils {
-    static public <T> List<T> reverse(List<T> list) {
+    // 列表任何类型都可以通过
+     public static <T> List<T> reverse(List<T> list) {
         List<T> result = new ArrayList<T>(list.size());
         ListIterator<T> it = list.listIterator(list.size());
         while (it.hasPrevious()) {
