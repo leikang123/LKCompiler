8593319e5dc913d912781c1e84b001e54054e9d5
leikang123 <1013626230@qq.com>
2022-03-30 18:19:33 +0800

    add code

---
+++ src/main/AST/LHSNode.java
@@ -0,0 +1,26 @@
+package main.AST;
+import main.type.Type;
+
+abstract public class LHSNode extends ExprNode {
+    protected Type type, origType;
+
+    public Type type() {
+        return type != null ? type : origType();
+    }
+
+    public void setType(Type t) {
+        this.type = t;
+    }
+
+    abstract protected Type origType();
+
+    public long allocSize() { return origType().allocSize(); }
+
+    public boolean isLvalue() { return true; }
+    public boolean isAssignable() { return isLoadable(); }
+
+    public boolean isLoadable() {
+        Type t = origType();
+        return !t.isArray() && !t.isFunction();
+    }
+}
