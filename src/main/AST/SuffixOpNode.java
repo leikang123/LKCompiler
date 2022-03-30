8e50886be30c35a88913736ca79652daa81566b9
leikang123 <1013626230@qq.com>
2022-03-30 18:23:29 +0800

    add code

---
+++ src/main/AST/SuffixOpNode.java
@@ -0,0 +1,11 @@
+package main.AST;
+
+public class SuffixOpNode extends UnaryArithmeticOpNode {
+    public SuffixOpNode(String op, ExprNode expr) {
+        super(op, expr);
+    }
+
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
