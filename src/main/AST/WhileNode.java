1b87a1a20f07ed08528c4f60e8e8565ae4809eed
leikang123 <1013626230@qq.com>
2022-03-30 18:27:32 +0800

    add code

---
+++ src/main/AST/WhileNode.java
@@ -0,0 +1,29 @@
+package main.AST;
+
+public class WhileNode extends StmtNode {
+    protected StmtNode body;
+    protected ExprNode cond;
+
+    public WhileNode(Location loc, ExprNode cond, StmtNode body) {
+        super(loc);
+        this.cond = cond;
+        this.body = body;
+    }
+
+    public ExprNode cond() {
+        return cond;
+    }
+
+    public StmtNode body() {
+        return body;
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("cond", cond);
+        d.printMember("body", body);
+    }
+
+    public <S,E> S accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
