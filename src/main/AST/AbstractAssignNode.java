17b2de074ca5624c1fabf64456c3e8032eebba4f
leikang123 <1013626230@qq.com>
2022-03-30 18:08:36 +0800

    add code

---
+++ src/main/AST/AbstractAssignNode.java
@@ -0,0 +1,37 @@
+package main.AST;
+import main.type.*;
+
+abstract public class AbstractAssignNode extends ExprNode {
+    ExprNode lhs, rhs;
+
+    public AbstractAssignNode(ExprNode lhs, ExprNode rhs) {
+        super();
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+
+    public Type type() {
+        return lhs.type();
+    }
+
+    public ExprNode lhs() {
+        return lhs;
+    }
+
+    public ExprNode rhs() {
+        return rhs;
+    }
+
+    public void setRHS(ExprNode expr) {
+        this.rhs = expr;
+    }
+
+    public Location location() {
+        return lhs.location();
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("lhs", lhs);
+        d.printMember("rhs", rhs);
+    }
+}
