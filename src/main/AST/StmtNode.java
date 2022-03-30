7cfacbd942f1630d5b624a66ebf1487317675176
leikang123 <1013626230@qq.com>
2022-03-30 18:21:19 +0800

    add code

---
+++ src/main/AST/StmtNode.java
@@ -0,0 +1,15 @@
+package main.AST;
+
+abstract public class StmtNode extends Node {
+    protected Location location;
+
+    public StmtNode(Location loc) {
+        this.location = loc;
+    }
+
+    public Location location() {
+        return location;
+    }
+
+    abstract public <S,E> S accept(ASTVisitor<S,E> visitor);
+}
