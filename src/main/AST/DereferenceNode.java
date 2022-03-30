461879398936278e6c5dc10f34ad9d127dca15b3
leikang123 <1013626230@qq.com>
2022-03-30 18:16:46 +0800

    add code

---
+++ src/main/AST/DereferenceNode.java
@@ -0,0 +1,37 @@
+package main.AST;
+import main.type.*;
+
+public class DereferenceNode extends LHSNode {
+    private ExprNode expr;
+
+    public DereferenceNode(ExprNode expr) {
+        this.expr = expr;
+    }
+
+    protected Type origType() {
+        return expr.type().baseType();
+    }
+
+    public ExprNode expr() {
+        return expr;
+    }
+
+    public void setExpr(ExprNode expr) {
+        this.expr = expr;
+    }
+
+    public Location location() {
+        return expr.location();
+    }
+
+    protected void _dump(Dumper d) {
+        if (type != null) {
+            d.printMember("type", type);
+        }
+        d.printMember("expr", expr);
+    }
+
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
