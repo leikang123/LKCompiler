d40a7cfa04a856d6badcec02127da2c7d0eaeb88
leikang123 <1013626230@qq.com>
2022-03-30 18:24:03 +0800

    add code

---
+++ src/main/AST/TypeDefinition.java
@@ -0,0 +1,37 @@
+package main.AST;
+import main.type.*;
+
+abstract public class TypeDefinition extends Node {
+    protected String name;
+    protected Location location;
+    protected TypeNode typeNode;
+
+    public TypeDefinition(Location loc, TypeRef ref, String name) {
+        this.name = name;
+        this.location = loc;
+        this.typeNode = new TypeNode(ref);
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Location location() {
+        return location;
+    }
+
+    public TypeNode typeNode() {
+        return typeNode;
+    }
+
+    public TypeRef typeRef() {
+        return typeNode.typeRef();
+    }
+
+    public Type type() {
+        return typeNode.type();
+    }
+
+    abstract public Type definingType();
+    abstract public <T> T accept(DeclarationVisitor<T> visitor);
+}
