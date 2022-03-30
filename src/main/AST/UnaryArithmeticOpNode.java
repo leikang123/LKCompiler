01f32c7abfa0fee79036e48c5aab350d75a23363
leikang123 <1013626230@qq.com>
2022-03-30 18:25:48 +0800

    add code

---
+++ src/main/AST/UnaryArithmeticOpNode.java
@@ -0,0 +1,22 @@
+package main.AST;
+
+public class UnaryArithmeticOpNode extends UnaryOpNode {
+    protected long amount;
+
+    public UnaryArithmeticOpNode(String op, ExprNode expr) {
+        super(op, expr);
+        amount = 1;
+    }
+
+    public void setExpr(ExprNode expr) {
+        this.expr = expr;
+    }
+
+    public long amount() {
+        return this.amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+}
