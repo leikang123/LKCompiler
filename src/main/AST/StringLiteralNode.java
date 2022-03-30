e57fdb8319c50f83dee666c1f3c7f7d7572d07b9
leikang123 <1013626230@qq.com>
2022-03-30 18:22:35 +0800

    add code

---
+++ src/main/AST/StringLiteralNode.java
@@ -0,0 +1,33 @@
+package main.AST;
+import main.type.TypeRef;
+import main.entity.ConstantEntry;
+
+public class StringLiteralNode extends LiteralNode {
+    protected String value;
+    protected ConstantEntry entry;
+
+    public StringLiteralNode(Location loc, TypeRef ref, String value) {
+        super(loc, ref);
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public ConstantEntry entry() {
+        return entry;
+    }
+
+    public void setEntry(ConstantEntry ent) {
+        entry = ent;
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("value", value);
+    }
+
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
