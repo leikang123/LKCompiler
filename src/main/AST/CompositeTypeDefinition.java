974211d4c00a8820b56f67eeddd89185ee756bf7
leikang123 <1013626230@qq.com>
2022-03-30 18:16:16 +0800

    add code

---
+++ src/main/AST/CompositeTypeDefinition.java
@@ -0,0 +1,28 @@
+package main.AST;
+import main.type.*;
+import java.util.*;
+
+abstract public class CompositeTypeDefinition extends TypeDefinition {
+    protected List<Slot> members;
+
+    public CompositeTypeDefinition(Location loc, TypeRef ref,
+                                   String name, List<Slot> membs) {
+        super(loc, ref, name);
+        members = membs;
+    }
+
+    public boolean isCompositeType() {
+        return true;
+    }
+
+    abstract public String kind();
+
+    public List<Slot> members() {
+        return members;
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("name", name);
+        d.printNodeList("members", members);
+    }
+}
