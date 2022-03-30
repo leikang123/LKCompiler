ab097ba9b0bc624d5c9988555d87d119e9278364
leikang123 <1013626230@qq.com>
2022-03-30 18:27:06 +0800

    add code

---
+++ src/main/AST/VariableNode.java
@@ -0,0 +1,66 @@
+package main.AST;
+import main.type.Type;
+import main.entity.Entity;
+import main.entity.DefinedVariable;
+
+public class VariableNode extends LHSNode {
+    private Location location;
+    private String name;
+    private Entity entity;
+
+    public VariableNode(Location loc, String name) {
+        this.location = loc;
+        this.name = name;
+    }
+
+    public VariableNode(DefinedVariable var) {
+        this.entity = var;
+        this.name = var.name();
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public boolean isResolved() {
+        return (entity != null);
+    }
+
+    public Entity entity() {
+        if (entity == null) {
+            throw new Error("VariableNode.entity == null");
+        }
+        return entity;
+    }
+
+    public void setEntity(Entity ent) {
+        entity = ent;
+    }
+
+    public TypeNode typeNode() {
+        return entity().typeNode();
+    }
+
+    public boolean isParameter() {
+        return entity().isParameter();
+    }
+
+    protected Type origType() {
+        return entity().type();
+    }
+
+    public Location location() {
+        return location;
+    }
+
+    protected void _dump(Dumper d) {
+        if (type != null) {
+            d.printMember("type", type);
+        }
+        d.printMember("name", name, isResolved());
+    }
+
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
