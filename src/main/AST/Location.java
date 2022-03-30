5577823a1cad42897520e46893e2be2448534917
leikang123 <1013626230@qq.com>
2022-03-30 18:20:57 +0800

    add code

---
+++ src/main/AST/Location.java
@@ -0,0 +1,47 @@
+package main.AST;
+import main.parser.Token;
+import main.utils.TextUtils;
+import java.util.*;
+
+public class Location {
+    protected String sourceName;
+    protected CflatToken token;
+
+    public Location(String sourceName, Token token) {
+        this(sourceName, new CflatToken(token));
+    }
+
+    public Location(String sourceName, CflatToken token) {
+        this.sourceName = sourceName;
+        this.token = token;
+    }
+
+    public String sourceName() {
+        return sourceName;
+    }
+
+    public CflatToken token() {
+        return token;
+    }
+
+    /** line number */
+    public int lineno() {
+        return token.lineno();
+    }
+
+    public int column() {
+        return token.column();
+    }
+
+    public String line() {
+        return token.includedLine();
+    }
+
+    public String numberedLine() {
+        return "line " + token.lineno() + ": " + line();
+    }
+
+    public String toString() {
+        return sourceName + ":" + token.lineno();
+    }
+}
