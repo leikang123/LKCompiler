2e6a8bb67aaa3bd396c8f072325f52ee8a7871d3
leikang123 <1013626230@qq.com>
2022-03-30 18:26:41 +0800

    add code

---
+++ src/main/AST/UnionNode.java
@@ -0,0 +1,27 @@
+package main.AST;
+import main.type.*;
+import java.util.*;
+
+public class UnionNode extends CompositeTypeDefinition {
+    public UnionNode(Location loc, TypeRef ref, String name, List<Slot> membs) {
+        super(loc, ref, name, membs);
+    }
+
+    public String kind() {
+        return "union";
+    }
+
+    public boolean isUnion() {
+        return true;
+    }
+
+    // #@@range/definingType{
+    public Type definingType() {
+        return new UnionType(name(), members(), location());
+    }
+    // #@@}
+
+    public <T> T accept(DeclarationVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
