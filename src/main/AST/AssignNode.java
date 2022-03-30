5314047021d4fac604a6596387d9e2d016150dd6
leikang123 <1013626230@qq.com>
2022-03-30 18:10:19 +0800

    add code

---
+++ src/main/AST/AssignNode.java
@@ -0,0 +1,11 @@
+package main.AST;
+
+public class AssignNode extends AbstractAssignNode {
+    public AssignNode(ExprNode lhs, ExprNode rhs) {
+        super(lhs, rhs);
+    }
+
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
