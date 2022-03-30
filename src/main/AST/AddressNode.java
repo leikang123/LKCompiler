6ab74d0c80f3cd58f6f1e2ad0730562719d2e574
leikang123 <1013626230@qq.com>
2022-03-30 18:09:13 +0800

    add code

---
+++ src/main/AST/AddressNode.java
@@ -0,0 +1,42 @@
+package main.AST;
+import main.type.Type;
+
+public class AddressNode extends ExprNode {
+    final ExprNode expr;
+    Type type;
+
+    public AddressNode(ExprNode expr) {
+        this.expr = expr;
+    }
+
+    public ExprNode expr() {
+        return expr;
+    }
+
+    public Type type() {
+        if (type == null) throw new Error("type is null");
+        return type;
+    }
+
+    /** Decides type of this node.
+     * This method is called from DereferenceChecker. */
+    public void setType(Type type) {
+        if (this.type != null) throw new Error("type set twice");
+        this.type = type;
+    }
+
+    public Location location() {
+        return expr.location();
+    }
+
+    protected void _dump(Dumper d) {
+        if (type != null) {
+            d.printMember("type", type);
+        }
+        d.printMember("expr", expr);
+    }
+
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
