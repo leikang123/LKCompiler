3b3ff65b592e3f761e17feb1529073467ea9e151
leikang123 <1013626230@qq.com>
2022-03-30 18:11:51 +0800

    add code

---
+++ src/main/AST/BreakNode.java
@@ -0,0 +1,14 @@
+package main.AST;
+
+public class BreakNode extends StmtNode {
+    public BreakNode(Location loc) {
+        super(loc);
+    }
+
+    protected void _dump(Dumper d) {
+    }
+
+    public <S,E> S accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
