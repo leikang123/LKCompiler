730d182bf6ae2a5590e67c101a51650856973da4
leikang123 <1013626230@qq.com>
2022-03-30 18:22:59 +0800

    add code

---
+++ src/main/AST/StructNode.java
@@ -0,0 +1,27 @@
+package main.ast;
+import main.type.*;
+import java.util.*;
+
+public class StructNode extends CompositeTypeDefinition {
+    public StructNode(Location loc, TypeRef ref, String name, List<Slot> membs) {
+        super(loc, ref, name, membs);
+    }
+
+    public String kind() {
+        return "struct";
+    }
+
+    public boolean isStruct() {
+        return true;
+    }
+
+    // #@@range/definingType{
+    public Type definingType() {
+        return new StructType(name(), members(), location());
+    }
+    // #@@}
+
+    public <T> T accept(DeclarationVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
