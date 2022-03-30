f732a509566ea806c91b5faac2de82f3c6e35858
leikang123 <1013626230@qq.com>
2022-03-30 18:26:17 +0800

    add code

---
+++ src/main/AST/UnaryOpNode.java
@@ -0,0 +1,50 @@
+package main.AST;
+import main.type.Type;
+
+public class UnaryOpNode extends ExprNode {
+    protected String operator;
+    protected ExprNode expr;
+    protected Type opType;
+
+    public UnaryOpNode(String op, ExprNode expr) {
+        this.operator = op;
+        this.expr = expr;
+    }
+
+    public String operator() {
+        return operator;
+    }
+
+    public Type type() {
+        return expr.type();
+    }
+
+    public void setOpType(Type t) {
+        this.opType = t;
+    }
+
+    public Type opType() {
+        return opType;
+    }
+
+    public ExprNode expr() {
+        return expr;
+    }
+
+    public void setExpr(ExprNode expr) {
+        this.expr = expr;
+    }
+
+    public Location location() {
+        return expr.location();
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("operator", operator);
+        d.printMember("expr", expr);
+    }
+
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
