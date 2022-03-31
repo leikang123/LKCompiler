87b4731d08eecea5e4c7101f3fbc712d82de3ddd
leikang123 <1013626230@qq.com>
2022-03-31 17:51:58 +0800

    update file

---
+++ src/main/AST/Slot.java
@@ -0,0 +1,59 @@
+package main.AST;
+import main.type.*;
+
+public class Slot extends Node {
+    protected TypeNode typeNode;
+    protected String name;
+    protected long offset;
+
+    public Slot(TypeNode t, String n) {
+        typeNode = t;
+        name = n;
+        offset = Type.sizeUnknown;
+    }
+
+    public TypeNode typeNode() {
+        return typeNode;
+    }
+
+    public TypeRef typeRef() {
+        return typeNode.typeRef();
+    }
+
+    public Type type() {
+        return typeNode.type();
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public long size() {
+        return type().size();
+    }
+
+    public long allocSize() {
+        return type().allocSize();
+    }
+
+    public long alignment() {
+        return type().alignment();
+    }
+
+    public long offset() {
+        return offset;
+    }
+
+    public void setOffset(long offset) {
+        this.offset = offset;
+    }
+
+    public Location location() {
+        return typeNode.location();
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("name", name);
+        d.printMember("typeNode", typeNode);
+    }
+}
