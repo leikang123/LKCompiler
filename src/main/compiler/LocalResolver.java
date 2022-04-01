4570858d4ff08c0d68e996cc1207b78def014c79
leikang123 <1013626230@qq.com>
2022-04-01 20:10:40 +0800

    update code

--- src/main/compiler/LocalResolver.java
+++ src/main/compiler/LocalResolver.java
@@ -4,11 +4,13 @@ import main.entity.*;
 import main.utils.ErrorHandler;
 import main.exception.*;
 import java.util.*;
-
+//变量引用的消解类
 public class LocalResolver extends Visitor {
-    // #@@range/ctor{
+    // 保存scope关系的栈
     private final LinkedList<Scope> scopeStack;
+    //管理字符串常量
     private final ConstantTable constantTable;
+    // 处理错误
     private final ErrorHandler errorHandler;
 
     public LocalResolver(ErrorHandler h) {
@@ -16,7 +18,6 @@ public class LocalResolver extends Visitor {
         this.scopeStack = new LinkedList<Scope>();
         this.constantTable = new ConstantTable();
     }
-    // #@@}
 
     private void resolve(StmtNode n) {
         n.accept(this);
@@ -26,24 +27,24 @@ public class LocalResolver extends Visitor {
         n.accept(this);
     }
 
-    // #@@range/resolve{
+    // 消解方法实现类的启动方法
     public void resolve(AST ast) throws SemanticException {
+        // 生成对象类的创建
         ToplevelScope toplevel = new ToplevelScope();
+        // 把生成类的对象添加到scopeStank里面，这样栈就有一个scope对象
         scopeStack.add(toplevel);
-
-        // #@@range/declareToplevel{
+        
+        // for遍历
         for (Entity decl : ast.declarations()) {
             toplevel.declareEntity(decl);
         }
         for (Entity ent : ast.definitions()) {
             toplevel.defineEntity(ent);
         }
-        // #@@}
-        // #@@range/resolveRefs{
         resolveGvarInitializers(ast.definedVariables());
         resolveConstantValues(ast.constants());
         resolveFunctions(ast.definedFunctions());
-        // #@@}
+    
         toplevel.checkReferences(errorHandler);
         if (errorHandler.errorOccured()) {
             throw new SemanticException("compile failed.");
@@ -52,9 +53,7 @@ public class LocalResolver extends Visitor {
         ast.setScope(toplevel);
         ast.setConstantTable(constantTable);
     }
-    // #@@}
-
-    // #@@range/resolveGvarInitializers{
+    
     private void resolveGvarInitializers(List<DefinedVariable> gvars) {
         for (DefinedVariable gvar : gvars) {
             if (gvar.hasInitializer()) {
@@ -62,7 +61,6 @@ public class LocalResolver extends Visitor {
             }
         }
     }
-    // #@@}
 
     private void resolveConstantValues(List<Constant> consts) {
         for (Constant c : consts) {
@@ -70,7 +68,7 @@ public class LocalResolver extends Visitor {
         }
     }
 
-    // #@@range/resolveFunctions{
+    
     private void resolveFunctions(List<DefinedFunction> funcs) {
         for (DefinedFunction func : funcs) {
             pushScope(func.parameters());
@@ -78,18 +76,14 @@ public class LocalResolver extends Visitor {
             func.setScope(popScope());
         }
     }
-    // #@@}
-
-    // #@@range/BlockNode{
+    
     public Void visit(BlockNode node) {
         pushScope(node.variables());
         super.visit(node);
         node.setScope(popScope());
         return null;
     }
-    // #@@}
-
-    // #@@range/pushScope{
+    
     private void pushScope(List<? extends DefinedVariable> vars) {
         LocalScope scope = new LocalScope(currentScope());
         for (DefinedVariable var : vars) {
@@ -103,28 +97,20 @@ public class LocalResolver extends Visitor {
         }
         scopeStack.addLast(scope);
     }
-    // #@@}
-
-    // #@@range/popScope{
+    
     private LocalScope popScope() {
         return (LocalScope)scopeStack.removeLast();
     }
-    // #@@}
-
-    // #@@range/currentScope{
+    
     private Scope currentScope() {
         return scopeStack.getLast();
     }
-    // #@@}
-
-    // #@@range/StringLiteralNode{
+   
     public Void visit(StringLiteralNode node) {
         node.setEntry(constantTable.intern(node.value()));
         return null;
     }
-    // #@@}
-
-    // #@@range/VariableNode{
+    
     public Void visit(VariableNode node) {
         try {
             Entity ent = currentScope().get(node.name());
@@ -136,7 +122,7 @@ public class LocalResolver extends Visitor {
         }
         return null;
     }
-    // #@@}
+    
 
     private void error(Node node, String message) {
         errorHandler.error(node.location(), message);
