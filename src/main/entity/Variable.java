a275b2e7b13ffe03e28ba6dbe4a32e5ea4fe22a9
leikang123 <1013626230@qq.com>
2022-04-01 20:12:28 +0800

    update code

--- src/main/entity/Variable.java
+++ src/main/entity/Variable.java
@@ -2,7 +2,7 @@ package main.entity;
 import main.AST.TypeNode;
 import main.type.*;
 
-abstract public class Variable extends Entity {
+public abstract  class Variable extends Entity {
     public Variable(boolean priv, TypeNode type, String name) {
         super(priv, type, name);
     }
