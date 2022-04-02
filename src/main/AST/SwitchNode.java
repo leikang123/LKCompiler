fb954618f9d3892eee774e42f3b61f5287729aee
leikang123 <1013626230@qq.com>
2022-04-02 17:56:51 +0800

    add code

---
+++ src/main/AST/SwitchNode.java
@@ -0,0 +1,30 @@
+package main.AST;
+import java.util.List;
+
+public class SwitchNode extends StmtNode {
+    protected ExprNode cond;
+    protected List<CaseNode> cases;
+
+    public SwitchNode(Location loc, ExprNode cond, List<CaseNode> cases) {
+        super(loc);
+        this.cond = cond;
+        this.cases = cases;
+    }
+
+    public ExprNode cond() {
+        return cond;
+    }
+
+    public List<CaseNode> cases() {
+        return cases;
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("cond", cond);
+        d.printNodeList("cases", cases);
+    }
+
+    public <S,E> S accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
