ca519921d556d84f0deca3ce454665e6d4d66810
leikang123 <1013626230@qq.com>
2022-04-02 17:56:31 +0800

    add code

---
+++ src/main/AST/ReturnNode.java
@@ -0,0 +1,26 @@
+package main.AST;
+
+public class ReturnNode extends StmtNode {
+    protected ExprNode expr;
+
+    public ReturnNode(Location loc, ExprNode expr) {
+        super(loc);
+        this.expr = expr;
+    }
+
+    public ExprNode expr() {
+        return this.expr;
+    }
+
+    public void setExpr(ExprNode expr) {
+        this.expr = expr;
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("expr", expr);
+    }
+
+    public <S,E> S accept(ASTVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+}
