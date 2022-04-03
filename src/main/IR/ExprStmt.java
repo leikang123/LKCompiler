cec34a93116b15db098e61937d53cff0d19ce4a3
leikang123 <1013626230@qq.com>
2022-04-03 17:47:28 +0800

    add ir code

---
+++ src/main/IR/ExprStmt.java
@@ -0,0 +1,23 @@
+package main.IR;
+import main.AST.*;
+
+public class ExprStmt extends Stmt {
+    protected Expr expr;
+
+    public ExprStmt(Location loc, Expr expr) {
+        super(loc);
+        this.expr = expr;
+    }
+
+    public Expr expr() {
+        return expr;
+    }
+
+    public <S,E> S accept(IRVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("expr", expr);
+    }
+}
