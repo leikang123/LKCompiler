be89f62a196603b220f00909506ce27ad35296ff
leikang123 <1013626230@qq.com>
2022-04-03 17:45:45 +0800

    add ir code

---
+++ src/main/IR/CJump.java
@@ -0,0 +1,38 @@
+package main.IR;
+import main.AST.*;
+import main.assembly.*;
+
+public class CJump extends Stmt {
+    protected Expr cond;
+    protected Label thenLabel;
+    protected Label elseLabel;
+
+    public CJump(Location loc, Expr cond, Label thenLabel, Label elseLabel) {
+        super(loc);
+        this.cond = cond;
+        this.thenLabel = thenLabel;
+        this.elseLabel = elseLabel;
+    }
+
+    public Expr cond() {
+        return cond;
+    }
+
+    public Label thenLabel() {
+        return thenLabel;
+    }
+
+    public Label elseLabel() {
+        return elseLabel;
+    }
+
+    public <S,E> S accept(IRVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("cond", cond);
+        d.printMember("thenLabel", thenLabel);
+        d.printMember("elseLabel", elseLabel);
+    }
+}
