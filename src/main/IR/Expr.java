05570734eb79192453ac6e26a301bd1ae0ff2a3d
leikang123 <1013626230@qq.com>
2022-04-03 17:47:10 +0800

    add ir code

---
+++ src/main/IR/Expr.java
@@ -0,0 +1,52 @@
+package main.IR;
+import main.type.*;
+import main.type.Type;
+import main.assembly.*;
+import main.entity.Entity;
+
+abstract public class Expr implements Dumpable {
+    final Type type;
+
+    Expr(Type type) {
+        this.type = type;
+    }
+
+    public Type type() { return type; }
+
+    public boolean isVar() { return false; }
+    public boolean isAddr() { return false; }
+
+    public boolean isConstant() { return false; }
+
+    public ImmediateValue asmValue() {
+        throw new Error("Expr#asmValue called");
+    }
+
+    public Operand address() {
+        throw new Error("Expr#address called");
+    }
+
+    public MemoryReference memref() {
+        throw new Error("Expr#memref called");
+    }
+
+    // #@@range/addressNode{
+    public Expr addressNode(Type type) {
+        throw new Error("unexpected node for LHS: " + getClass());
+    }
+    // #@@}
+
+    public Entity getEntityForce() {
+        return null;
+    }
+
+    abstract public <S,E> E accept(IRVisitor<S,E> visitor);
+
+    public void dump(Dumper d) {
+        d.printClass(this);
+        d.printMember("type", type);
+        _dump(d);
+    }
+
+    abstract protected void _dump(Dumper d);
+}
