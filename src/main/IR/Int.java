0562f379be48da65f20f13bf81ba2857e69e806d
leikang123 <1013626230@qq.com>
2022-04-03 17:48:05 +0800

    add ir code

---
+++ src/main/IR/Int.java
@@ -0,0 +1,31 @@
+package main.IR;
+import main.assembly.*;
+
+public class Int extends Expr {
+    protected long value;
+
+    public Int(Type type, long value) {
+        super(type);
+        this.value = value;
+    }
+
+    public long value() { return value; }
+
+    public boolean isConstant() { return true; }
+
+    public ImmediateValue asmValue() {
+        return new ImmediateValue(new IntegerLiteral(value));
+    }
+
+    public MemoryReference memref() {
+        throw new Error("must not happen: IntValue#memref");
+    }
+
+    public <S,E> E accept(IRVisitor<S,E> visitor) {
+        return visitor.visit(this);
+    }
+
+    protected void _dump(Dumper d) {
+        d.printMember("value", value);
+    }
+}
