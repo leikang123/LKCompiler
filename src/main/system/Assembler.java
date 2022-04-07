e3d9e27928e839be610294f569cfa39dacb29f58
leikang123 <1013626230@qq.com>
2022-04-07 11:15:06 +0800

    update code

--- src/main/system/Assembler.java
+++ src/main/system/Assembler.java
@@ -1,5 +1,7 @@
 package main.system;
 
+import main.exception.IPCException;
+
 public interface Assembler {
     void assemble(String srcPath, String destPath,
             AssemblerOptions opts) throws IPCException;
