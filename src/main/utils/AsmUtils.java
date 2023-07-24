09743baa4c6eb2314af031e060ec3108860a5122
kangshifu <42248685+leikang123@users.noreply.github.com>
2023-07-24 15:50:01 +0800

    Update AsmUtils.java

--- src/main/utils/AsmUtils.java
+++ src/main/utils/AsmUtils.java
@@ -1,7 +1,7 @@
 package main.utils;
 
 public final class AsmUtils {
-        static public long align(long n, long alignment) {
+      public  static  long align(long n, long alignment) {
             return (n + alignment - 1) / alignment * alignment;
         }
 }
